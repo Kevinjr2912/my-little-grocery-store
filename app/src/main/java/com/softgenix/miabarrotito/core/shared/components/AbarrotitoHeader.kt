ce76b21ce64a35724eaaed2de4f6c1174c12ff72
Andresito126 <159158192+Andresito126@users.noreply.github.com>
2026-02-05 03:11:28 -0600

    Add: Header reusable component added

---
+++ app/src/main/java/com/softgenix/miabarrotito/core/shared/components/AbarrotitoHeader.kt
@@ -0,0 +1,119 @@
+package com.softgenix.miabarrotito.core.shared.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.softgenix.miabarrotito.R
+
+@Composable
+fun AbarrotitoHeader(
+    title: String ,
+    subtitle: String? = null,
+    iconLeft: ImageVector? = Icons.Default.Person,
+    iconRight: ImageVector? = Icons.Default.Settings,
+    canNavigateBack: Boolean = false,
+    onBackClick: () -> Unit = {},
+    onRightIconClick: () -> Unit = {}
+) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (canNavigateBack) {
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Regresar"
+                )
+            }
+        }
+
+        iconLeft?.let {
+            Box(
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape)
+                    .background(Color.LightGray.copy(alpha = 0.3f)),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = it,
+                    contentDescription = "Perfil",
+                    modifier = Modifier.size(30.dp),
+                    tint = Color.DarkGray
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Column(modifier = Modifier.weight(1f)) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.headlineSmall.copy(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 22.sp
+                )
+            )
+            //opcional 1ra forma
+            if (subtitle != null) {
+                Text(
+                    text = subtitle,
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = Color.Gray
+                )
+            }
+        }
+
+        //opcional 2da forma
+        iconRight?.let {
+            IconButton(onClick = onRightIconClick) {
+                Icon(
+                    imageVector = it,
+                    contentDescription = "derecha",
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewAbarrotitoHeader() {
+    AbarrotitoHeader(
+        title = "Configuración",
+        iconLeft = null,
+        canNavigateBack = true,
+        iconRight = ImageVector.vectorResource(id = R.drawable.tabler_icon_library_plus),
+        onBackClick = {
+            println("nav")
+        },
+        onRightIconClick = {
+            println("nva")
+        }
+    )
+}
+
